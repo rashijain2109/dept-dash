784b83676523b0ee9c610304fd554840b807d1ba
Daksh <dyashlaha039909@gmail.com>
2020-01-27 19:43:07 +0530

    Frontend: Add project form

--- frontend/src/views/UserProfile.jsx
+++ frontend/src/views/UserProfile.jsx
@@ -22,7 +22,7 @@ import {
   Col,
   FormGroup,
   FormLabel,
-  FormControl
+  FormControl,
 } from "react-bootstrap";
 
 import { Card } from "../components/Card/Card.jsx";
@@ -38,143 +38,96 @@ class UserProfile extends Component {
       <div className="content">
         <Container fluid>
           <Row>
-            <Col md={8}>
+            <Col md={12}>
               <Card
-                title="Edit Profile"
+                title="Add Project"
                 content={
                   <form>
-                    <FormInputs
-                      ncols={["col-md-5", "col-md-3", "col-md-4"]}
-                      properties={[
-                        {
-                          label: "Department (disabled)",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Company",
-                          defaultValue: "Creative Code Inc.",
-                          disabled: true
-                        },
-                        {
-                          label: "Username",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Username",
-                        },
-                        {
-                          label: "Email address",
-                          type: "email",
-                          bsClass: "form-control",
-                          placeholder: "Email"
-                        }
-                      ]}
-                    />
-                    <FormInputs
-                      ncols={["col-md-6", "col-md-6"]}
-                      properties={[
-                        {
-                          label: "First name",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "First name",
-                          defaultValue: "Mike"
-                        },
-                        {
-                          label: "Last name",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Last name",
-                          defaultValue: "Andrew"
-                        }
-                      ]}
-                    />
-                    <FormInputs
-                      ncols={["col-md-12"]}
-                      properties={[
-                        {
-                          label: "Adress",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Home Adress",
-                          defaultValue:
-                            "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09"
-                        }
-                      ]}
-                    />
-                    <FormInputs
-                      ncols={["col-md-4", "col-md-4", "col-md-4"]}
-                      properties={[
-                        {
-                          label: "City",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "City",
-                          defaultValue: "Mike"
-                        },
-                        {
-                          label: "Country",
-                          type: "text",
-                          bsClass: "form-control",
-                          placeholder: "Country",
-                          defaultValue: "Andrew"
-                        },
-                        {
-                          label: "Postal Code",
-                          type: "number",
-                          bsClass: "form-control",
-                          placeholder: "ZIP Code"
-                        }
-                      ]}
-                    />
+                    <Row>
+                      <div className="col-md-6" key={1}>
+                      <FormGroup>
+                        <FormLabel>Select Authors</FormLabel>
+                        <FormControl as="select" multiple>
+                          <option>Daksh Yashlaha</option>
+                          <option>Rashi Jain</option>
+                          <option>Nihal Jain</option>
+                          <option>Pranjal Gupta</option>
+                          <option>Ujjwal Raizada</option>
+                          <option>Krut Patel</option>
+                        </FormControl>
+                      </FormGroup>
+                      </div>
+
+                      <div className="col-md-6" key={2}>
+                      <FormGroup>
+                        <FormLabel>Proposed by</FormLabel>
+                        <FormControl as="select" multiple>
+                          <option>Daksh Yashlaha</option>
+                          <option>Rashi Jain</option>
+                        </FormControl>
+                      </FormGroup>
+                      </div>
+                    </Row>
 
                     <Row>
-                      <Col md={12}>
-                        <FormGroup controlId="formControlsTextarea">
-                          <FormLabel>About Me</FormLabel>
-                          <FormControl
-                            rows="5"
-                            componentClass="textarea"
-                            bsClass="form-control"
-                            placeholder="Here can be your description"
-                            defaultValue="Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo."
-                          />
-                        </FormGroup>
-                      </Col>
+                      <div className="col-md-8" key={1}>
+                      <FormGroup>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl type="text" />
+                      </FormGroup>
+                      </div>
+
+                      <div className="col-md-4" key={2}>
+                      <FormGroup>
+                        <FormLabel>Status</FormLabel>
+                        <FormControl as="select">
+                          <option>Communicated</option>
+                          <option>Rejected</option>
+                          <option>Accepted</option>
+                        </FormControl>
+                      </FormGroup>
+                      </div>
                     </Row>
+
+                    <Row>
+                      <div className="col-md-12" key={1}>
+                      <FormGroup>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl as="textarea" rows={3} />
+                      </FormGroup>
+                      </div>
+                    </Row>
+
+                    <Row>
+                      <div className="col-md-6" key={1}>
+                      <FormGroup>
+                      <FormLabel>Agency</FormLabel>
+                      <FormControl type="text" />
+                      </FormGroup>
+                      </div>
+
+                      <div className="col-md-6" key={2}>
+                      <FormGroup>
+                        <FormLabel>Scheme</FormLabel>
+                        <FormControl type="text" />
+                      </FormGroup>
+                      </div>
+                    </Row>
+
+                    <Row>
+                      <div className="col-md-12" key={1}>
+                      <FormGroup>
+                        <FormLabel>Principal Investigator / Co-Investigator</FormLabel>
+                        <FormControl type="text" />
+                      </FormGroup>
+                      </div>
+                    </Row>
+
                     <Button bsStyle="info" pullRight fill type="submit">
-                      Update Profile
+                      Submit
                     </Button>
                     <div className="clearfix" />
                   </form>
-                }
-              />
-            </Col>
-            <Col md={4}>
-              <UserCard
-                bgImage="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400"
-                avatar={avatar}
-                name="Mike Andrew"
-                userName="michael24"
-                description={
-                  <span>
-                    "Lamborghini Mercy
-                    <br />
-                    Your chick she so thirsty
-                    <br />
-                    I'm in that two seat Lambo"
-                  </span>
-                }
-                socials={
-                  <div>
-                    <Button simple>
-                      <i className="fa fa-facebook-square" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-twitter" />
-                    </Button>
-                    <Button simple>
-                      <i className="fa fa-google-plus-square" />
-                    </Button>
-                  </div>
                 }
               />
             </Col>
